b1b7d5a641dd69af5ad93c140adc980bcd94fc04
leeyugyeong <lyg4910@naver.com>
2023-01-17 17:43:05 +0900

    20230117

--- 20230117/test.js
+++ 20230117/test.js
@@ -1,2 +1,25 @@
-const a=1; 
-console.log(a);
+const arr=[1,2,3,1,2,,1,2,3];
+let count=0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==1){
+        count=count+1;
+    }
+}
+
+console.log(count)
+
+const arr2=['A','E','W','Q','A'];
+let d=0;
+for(let i=0;i<arr2.length;i++){
+    if(arr2[i]==='A'){
+        d=1;
+        break;
+    }
+}
+
+if(d===0){
+    console.log("X");
+}
+else{
+    console.log("O");
+}
